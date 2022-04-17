891f85c60ec15556566193a8473606d9ef588a96
Marcelo Agustín Villagrán <magustin.villagran@gmail.com>
2022-04-16 21:28:04 -0300

    db_url config

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3333" ;
+axios.defaults.baseURL = process.env.REACT_APP_API? process.env.REACT_APP_API : process.env.DB_URL ;
 
 render(
   <React.StrictMode>
